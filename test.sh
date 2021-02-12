68804d79e204386e25725c61c572a14899bf1ff5
j1mb0o <66755426+j1mb0o@users.noreply.github.com>
2021-02-13 00:28:08 +0200

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+now="$(date +"%H")"		#Get the current time
+
+#echo "The user is $USER - $now"
+
+if [ $now -lt 12 ] ; then
+	zenity --notification --text "Good Morning $USER"
+	echo "Good Morning $USER"
+elif [ $now -lt 18 ] ; then
+	zenity --notification --text "Good Afternoon $USER"
+	echo "Good Afternoon $USER"
+else
+	zenity --notification --text "Good Evening $USER"
+	echo "Good Evening $USER"
+fi
+#zenity --notification --text "System Update neccessary $USER"
