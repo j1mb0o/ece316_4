03ac6190e2d7018eb3861df95555d1c45184b34d
j1mb0o <66755426+j1mb0o@users.noreply.github.com>
2021-02-15 09:42:42 +0200

    Add files via upload

--- ex4.sh
+++ ex4.sh
@@ -3,20 +3,31 @@
 declare -a inp
 inp=("$@")
 
-#for ((k=1; k<$#; k++))
+#for ((k=0; k<$#; k++))
 #do
 #	echo ${inp[$k]}
+
 #done
+#echo ${inp[-1]}
 
 if [ ${1:0:1} == '-' ]; then #tto to if ginete an to proto gramma/xaraktira einai - diladi kapia parametros
-for ((i=1; i<$#; i++))
-do
-	grep $1  ${inp[i]} ${inp[-1]}
-done
+	if [ $1 == '-r' ]; then
+		for filename in *.txt ; do
+			grep $2 $filename
+		done
+	else
 
+		for ((i=1; i<$#; i++))
+		do
+			grep $1  ${inp[i]} ${inp[-1]}
+		done
+	fi
 else 
-for ((i=0; i<$#; i++))
-do
-	grep ${inp[i]} ${inp[-1]}
-done
+	for ((i=0; i<$#; i++))
+	do
+		grep ${inp[i]} ${inp[-1]}
+	done
 fi
+
+
+#echo ${inp[-1]}
